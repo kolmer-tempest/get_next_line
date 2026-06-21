4e289c8358eaebc422800df878e3ef1ffe9eb627
kolmer-tempest <bkolmer@outlook.pt>
2026-06-21 23:59:22 +0100

    colocar struct

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,33 @@
 /*   By: bpires-k <bpires-k@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 21:42:46 by bpires-k          #+#    #+#             */
-/*   Updated: 2026/06/14 12:06:45 by bpires-k         ###   ########.fr       */
+/*   Updated: 2026/06/21 17:22:42 by bpires-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	*save_line(int fd, char *str)
+int	save_line(int fd, char *str)
 {
-	char	*buffer;
+	t_list	*buffer;
 	int		chars_read;
 
-	buffer = (char *) malloc(BUFFER_SIZE);
-	bytes_read = 1;
-	while (bytes_read != 0 || ft_strchr(buffer, '\n') != NULL)
+	buffer = (t_list) malloc(sizeof(t_lis) * BUFFER_SIZE + 1);
+	chars_read = 1;
+	while (chars_read != 0 || ft_strchr(buffer, '\n') != NULL)
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		buffer[bytes_read] = '\0';
-		str = buffer;
+		chars_read = read(fd, buffer, BUFFER_SIZE);
+		buffer[chars_read] = '\0';
+		str += buffer;
 		free(buffer);
 	}
-	return (str);
+	return (chars_read);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*line;
-	int chars_read;
+	static t_list	*line = NULL;
+	int				chars_read;
 
 	chars_read = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bpires-k <bpires-k@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 21:43:08 by bpires-k          #+#    #+#             */
-/*   Updated: 2026/06/09 21:48:21 by bpires-k         ###   ########.fr       */
+/*   Updated: 2026/06/21 17:23:35 by bpires-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_list
+{
+	char			*buf_str;
+	struct s_list	*next;
+}			t_list;
+
 char	*get_next_line(int fd);
-char	*save_line(int fd, char *str);
+int		save_line(int fd, char *str);
 char	*ft_strchr(const char *s, int c);
 
 #endif
